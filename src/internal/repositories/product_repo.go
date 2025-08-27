a58aaa497e1cd8a2252ac16c4122a23bd9234d28
MintzyG <ericbraga2000@gmail.com>
2025-08-26 21:24:47 -0300

    Created GetGlobalUserProductsFromID

--- src/internal/repositories/product_repo.go
+++ src/internal/repositories/product_repo.go
@@ -695,3 +695,11 @@ func (r *ProductRepo) GetAllActivitiesFromEvent(eventID string) ([]models.Activi
 	}
 	return activities, nil
 }
+
+func (r *ProductRepo) GetGlobalUserProductsFromID(id string) ([]models.UserProduct) {
+	var userProducts []models.UserProduct
+	if err := r.DB.Where("product_id = ?", id).Find(&userProducts).Error; err != nil {
+		return nil, err
+	}
+	return userProducts, nil
+}
